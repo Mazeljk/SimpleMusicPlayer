5d6d07efdeaddc2fbb853dd16177abe081034d30
maze LI <mazeljk@gmail.com>
2019-03-30 23:44:52 +0100

    first edit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8- -*-
+
 import os
 import sys
 import time
@@ -17,7 +20,7 @@ class musicplayer(QWidget):
 
     def __initialization(self):
         self.setWindowTitle('MusicPlayer V0.1')
-        self.setWindowIcon()  # TODO: set Icon
+        self.setWindowIcon(QIcon())  # TODO: set Icon
         self.song_list = []
         self.song_formats = ['mp3', 'm4a', 'wav', 'flac']
         self.configfile = 'configuration.ini'
@@ -27,9 +30,21 @@ class musicplayer(QWidget):
         self.switch = False
         self.pause = False
 
+        # playing time
+        self.lable1 = QLabel('00:00')
+        self.label1.setStyle(QStyleFactory.create('Fusion'))
+        self.lable2 = QLable('00:00')
+        self.lable2.setStyle(QStyleFactory.create('Fusion'))
+
+        # slider
+        self.slider = QSlider(Qt.Horizontal, self)
+        self.slider.sliderMoved[int].connect(
+            lambda: self.player.setPosition(self.slider.value()))
+        self.slider.setStyle(QStyleFactory.create('Fusion'))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mod = musicplayer()
-    mod.show()
+    GUI = musicplayer()
+    GUI.show()
     sys.exit(app.exec_)
